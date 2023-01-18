ac7590b0ef79d778523b2cd2ccae69431a5742c4
Siddhi Bandivdekar <bandivdekarsiddhi@gmail.com>
2023-01-19 04:53:43 +0530

    Usestate for App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [boardData, setBoardData] = useState(
+    JSON.parse(localStorage.getItem("board-data"))
+  );
+  const [message, setMessage] = useState(null);
+  const [error, setError] = useState(false);
+  const [charArray, setCharArray] = useState([]);
+
   return <div className="App"></div>;
 }
 
