bb927dc0ab53960e68f7b29be5dcb5d43b026cb8
Siddhi Bandivdekar <bandivdekarsiddhi@gmail.com>
2023-01-19 05:06:35 +0530

    Reset board code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import { wordList } from "./constants/data";
 
 function App() {
   const [boardData, setBoardData] = useState(
@@ -8,6 +9,26 @@ function App() {
   const [message, setMessage] = useState(null);
   const [error, setError] = useState(false);
   const [charArray, setCharArray] = useState([]);
+
+  const resetBoard = () => {
+    const alphabetIndex = Math.floor(Math.random() * 26);
+    const wordIndex = Math.floor(
+      Math.random() * wordList[String.fromCharCode(97 + alphabetIndex)].length
+    );
+    let newBoardData = {
+      ...boardData,
+      solution: wordList[String.fromCharCode(97 + alphabetIndex)][wordIndex],
+      rowIndex: 0,
+      boardWords: [],
+      boardRowStatus: [],
+      presentCharArray: [],
+      absentCharArray: [],
+      correctCharArray: [],
+      status: "IN_PROGRESS",
+    };
+    setBoardData(newBoardData);
+    localStorage.setItem("board-data", JSON.stringify(newBoardData));
+  };
 
   return <div className="App"></div>;
 }
