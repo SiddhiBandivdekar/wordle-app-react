e4b62d0ecbe99a18ea484b5a745467525d06f6c7
Siddhi Bandivdekar <bandivdekarsiddhi@gmail.com>
2023-01-20 18:15:04 +0530

    Keyboard component

---
+++ src/components/Keyboard/Keyboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { keys } from "../../constants/constants";
+import "./Keyboard.css";
+
+const Keyboard = () => {
+  return <div>Keyboard</div>;
+};
+
+export default Keyboard;
