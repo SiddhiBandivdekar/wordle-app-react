323c8aefacc02d91d5b489053836935549e23029
Siddhi Bandivdekar <bandivdekarsiddhi@gmail.com>
2023-01-20 17:53:38 +0530

    Updated the code

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-import { useState } from "react";
 import "./App.css";
-import { wordList } from "./constants/data";
+import Wordle from "./Wordle/Wordle";
 
-function App() {
-  const [boardData, setBoardData] = useState(
-    JSON.parse(localStorage.getItem("board-data"))
+const App = () => {
+  return (
+    <div>
+      <Wordle />
+    </div>
   );
-  const [message, setMessage] = useState(null);
-  const [error, setError] = useState(false);
-  const [charArray, setCharArray] = useState([]);
-
-  const resetBoard = () => {
-    const alphabetIndex = Math.floor(Math.random() * 26);
-    const wordIndex = Math.floor(
-      Math.random() * wordList[String.fromCharCode(97 + alphabetIndex)].length
-    );
-    let newBoardData = {
-      ...boardData,
-      solution: wordList[String.fromCharCode(97 + alphabetIndex)][wordIndex],
-      rowIndex: 0,
-      boardWords: [],
-      boardRowStatus: [],
-      presentCharArray: [],
-      absentCharArray: [],
-      correctCharArray: [],
-      status: "IN_PROGRESS",
-    };
-    setBoardData(newBoardData);
-    localStorage.setItem("board-data", JSON.stringify(newBoardData));
-  };
-
-  return <div className="App"></div>;
-}
+};
 
 export default App;
